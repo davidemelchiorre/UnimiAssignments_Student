ae4072e9c05d234e4146cae32c6e00f2f5c72d24
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-21 11:19:11 +0100

    Update init.py

--- init.py
+++ init.py
@@ -2,7 +2,7 @@ import sys
 import subprocess
 
 print("Inserisci codice di installazione:")
-username=input()
+username=raw_input()
 
 subprocess.call("git clone C:\\Users\\Davide\\Desktop\\unimi\\students\\"+username+"\\bare.git Personali",shell=True)
 subprocess.call("git clone C:\\Users\\Davide\\Desktop\\unimi\\public_notebooks Esercitazioni",shell=True)
